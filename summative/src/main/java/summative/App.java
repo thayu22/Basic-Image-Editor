611aac9bd15b6e4b2e4d0ae4f31e2827f65740b0
Thayu Logan <thayurantbt@gmail.com>
2026-01-18 19:05:20 -0500

    Add files via upload

---
+++ summative/src/main/java/summative/App.java
@@ -0,0 +1,26 @@
+package summative;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("primary.fxml"));
+        Parent root = loader.load();
+        PrimaryController controller = loader.getController();
+        controller.setStage(primaryStage);
+        primaryStage.setTitle("Summative");
+        primaryStage.setScene(new Scene(root, 750, 750));
+        primaryStage.show();
+
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
